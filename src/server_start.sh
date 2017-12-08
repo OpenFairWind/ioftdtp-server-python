8149459ed6bb8d843641387b3c4bd2a566c97ff2
MarioRuggieri <marioruggieri94@gmail.com>
2017-12-08 16:26:55 +0000

    starter script calls classpath generation script and supports app config file

--- src/server_start.sh
+++ src/server_start.sh
@@ -2,4 +2,7 @@
 
 export FLASK_APP="server_api.py"
 export FLASK_DEBUG=1
+export YOURAPPLICATION_SETTINGS=PATH_TO_CFG_FILE
+source PATH_TO_SOURCEME_FOR_JAVA_PROCESSES
+
 python -m flask run --host=0.0.0.0 --port=5050 --with-threads
