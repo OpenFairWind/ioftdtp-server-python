f965b1017b03a42e6489ed793a0dc0f059e07ace
MarioRuggieri <marioruggieri94@gmail.com>
2017-12-11 16:41:14 +0000

    cfg file passed from the command line

--- src/server_start.sh
+++ src/server_start.sh
@@ -2,7 +2,6 @@
 
 export FLASK_APP="server_api.py"
 export FLASK_DEBUG=1
-export YOURAPPLICATION_SETTINGS=PATH_TO_CFG_FILE
-source PATH_TO_SOURCEME_FOR_JAVA_PROCESSES
+export YOURAPPLICATION_SETTINGS=$1
 
 python -m flask run --host=0.0.0.0 --port=5050 --with-threads
